354a719c6ee6573960b03f4780bf426edd3dbf59
NyCode <me@nycode.dev>
2021-12-02 15:26:35 +0100

    Optimize day one solution

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,30 +1,19 @@
 use std::fs;
 
 fn main() {
-    let numbers = read_input();
-    let count = count_increasements(&numbers);
-    println!("{}", count);
-
-    let count = count_increasements(
-        &numbers
-            .windows(3)
-            .map(|window| window[0] + window[1] + window[2])
-            .collect::<Vec<i32>>(),
-    );
-    println!("{}", count);
-}
-
-fn count_increasements(numbers: &[i32]) -> usize {
-    numbers
-        .windows(2)
-        .filter(|window| window[0] < window[1])
-        .count()
-}
-
-fn read_input() -> Vec<i32> {
-    fs::read_to_string("inputs/day01.txt")
+    let numbers: Vec<i32> = fs::read_to_string("inputs/day01.txt")
         .expect("Unable to read input")
         .lines()
         .filter_map(|line| line.parse::<i32>().ok())
-        .collect()
+        .collect();
+
+    let first = numbers
+        .windows(2)
+        .filter(|window| window.first() < window.last())
+        .count();
+    let second = numbers
+        .windows(4)
+        .filter(|window| window.first() < window.last())
+        .count();
+    println!("First Solution: {}\nSecond Solution: {}", first, second);
 }
