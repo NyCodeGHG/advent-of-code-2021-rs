c6909a2e14c68e43ef82bf35e3cd7af6a148b3bd
NyCode <me@nycode.dev>
2021-12-02 17:06:21 +0100

    Add solution for part 2 of day 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,16 +1,29 @@
 use std::fs;
 
 fn main() {
-    let (depth, pos) = fs::read_to_string("inputs/day02.txt")
+    let actions: Vec<Action> = fs::read_to_string("inputs/day02.txt")
         .expect("Unable to read input")
         .lines()
         .filter_map(|line| Action::from_string(line))
+        .collect();
+    let (depth, pos) = actions
+        .iter()
         .fold((0, 0), |(depth, pos), action| match action {
             Action::Forward(n) => (depth, pos + n),
             Action::Down(n) => (depth + n, pos),
             Action::Up(n) => (depth - n, pos),
         });
     println!("Depth * Horizontal Position: {}", depth * pos);
+
+    let (depth, pos, _) =
+        actions
+            .iter()
+            .fold((0, 0, 0), |(depth, pos, aim), action| match action {
+                Action::Forward(n) => (depth + aim * n, pos + n, aim),
+                Action::Down(n) => (depth, pos, aim + n),
+                Action::Up(n) => (depth, pos, aim - n),
+            });
+    println!("Depth * Horizontal Position (Aim Method): {}", depth * pos);
 }
 
 enum Action {
