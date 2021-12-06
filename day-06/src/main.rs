43eb72d6f6126171dd477d7563ddfaafc5d35c26
NyCode <me@nycode.dev>
2021-12-06 12:40:02 +0100

    Add solution for part 2 of day 6. Aka rewrite everything

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,49 +1,39 @@
+use itertools::Itertools;
+use std::collections::HashMap;
 use std::fs;
-use std::fmt::Debug;
 
 fn main() {
-    let mut fishes: Vec<Lanternfish> = fs::read_to_string("inputs/day06.txt")
+    let fishes: Vec<u8> = fs::read_to_string("inputs/day06.txt")
         .expect("Unable to read input")
         .lines()
         .next()
         .unwrap()
         .split(',')
         .map(|v| v.parse::<u8>().unwrap())
-        .map(|value| Lanternfish { value })
         .collect();
 
-    for _ in 0..80 {
-        let mut new_fishes = vec![];
-        for fish in fishes.iter_mut() {
-            let result = fish.next_day();
-            if result.is_some() {
-                new_fishes.push(result.unwrap());
-            }
-        }
-        fishes.append(&mut new_fishes);
-    }
-    println!("{}", fishes.len())
+    let first_solution = simulate(80, &fishes);
+    let second_solution = simulate(256, &fishes);
+    println!("{}", first_solution);
+    println!("{}", second_solution);
 }
 
-struct Lanternfish {
-    value: u8,
-}
-
-impl Debug for Lanternfish {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(&self.value.to_string())
-    }
-}
-
-impl Lanternfish {
-    fn next_day(&mut self) -> Option<Lanternfish> {
-        if self.value == 0 {
-            self.value = 6;
-            return Some(Lanternfish {
-                value: 8
+fn simulate(days: u32, fishes: &[u8]) -> usize {
+    let mut fishes: HashMap<u8, usize> = fishes.iter().cloned().counts();
+    for _ in 0..days {
+        fishes = fishes
+            .iter()
+            .sorted()
+            .rev()
+            .fold(HashMap::new(), |mut fishes, (fish, amount)| {
+                if *fish == 0 {
+                    fishes.insert(8, *amount);
+                    fishes.insert(6, fishes.get(&6).unwrap_or(&0) + *amount);
+                } else {
+                    fishes.insert(*fish - 1, *amount);
+                }
+                fishes
             });
-        }
-        self.value = self.value - 1;
-        None
     }
+    fishes.values().sum()
 }
