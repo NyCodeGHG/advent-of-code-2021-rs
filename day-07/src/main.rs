6a21c1b3c58bab41c0ee3f5598e1b7af622e1d38
NyCode <me@nycode.dev>
2021-12-07 12:51:09 +0100

    Add time measurement to day 7

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
-use std::fs;
 use itertools::MinMaxResult::MinMax;
+use std::{fs, time::SystemTime};
 
 fn main() {
     let crabs: Vec<i32> = fs::read_to_string("inputs/day07.txt")
@@ -11,21 +11,22 @@ fn main() {
         .split(',')
         .filter_map(|v| v.parse::<i32>().ok())
         .collect();
+    let now = SystemTime::now();
     let (fuel, pos) =
         calculate_fuel(&crabs, |crab, pos| i32::abs(crab - pos)).expect("No solution found");
     println!("Position: {}, Fuel: {}", pos, fuel);
-    let (fuel, pos) =
-        calculate_fuel(&crabs, fuel_steps).expect("No solution found");
+    let (fuel, pos) = calculate_fuel(&crabs, fuel_steps).expect("No solution found");
     println!("Position: {}, Fuel: {}", pos, fuel);
+    println!("Solution took {}ms", now.elapsed().unwrap().as_millis());
 }
 
 fn calculate_fuel<F>(crabs: &[i32], fuel_algorithm: F) -> Option<(i32, i32)>
-    where
-        F: Fn(i32, i32) -> i32,
+where
+    F: Fn(i32, i32) -> i32,
 {
     let positions = match crabs.iter().minmax() {
         MinMax(a, b) => ((*a)..=(*b)).collect_vec(),
-        _ => return None
+        _ => return None,
     };
     positions
         .iter()
