3d318c53db2378ae23a6697219961d5f5f619f0c
NyCode <me@nycode.dev>
2021-12-09 16:44:21 +0100

    Add solution for part 2 of day 9

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,72 +1,34 @@
+use itertools::{iproduct, Itertools};
+use pathfinding::prelude::{bfs_reach, Matrix};
 use std::fs;
 
 fn main() {
     let input = fs::read_to_string("inputs/day09.txt").expect("Unable to read input");
-    let length = input.lines().next().unwrap().len();
-    let numbers: Vec<u32> = input
-        .lines()
-        .flat_map(|l| {
-            l.chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect::<Vec<u32>>()
-        })
-        .collect();
-    let sum: u32 = numbers
-        .iter()
-        .enumerate()
-        .filter(|(i, number)| {
-            get_neighbours(&numbers, *i, length)
-                .iter()
-                .all(|n| n > *number)
-        })
-        .map(|(_, n)| n + 1)
-        .sum();
-    println!("{}", sum);
+    println!("{}", part1(&input));
+    println!("{}", part2(&input));
 }
 
-fn get_neighbours(numbers: &[u32], index: usize, length: usize) -> Vec<u32> {
-    let row_index = index % length;
-    let left = if row_index != 0 {
-        numbers.get(index - 1)
-    } else {
-        None
-    };
-    let top = if index >= length {
-        numbers.get(index - length)
-    } else {
-        None
-    };
-    let right = if row_index != length - 1 {
-        numbers.get(index + 1)
-    } else {
-        None
-    };
-    let bottom = numbers.get(index + length);
-    left.iter()
-        .chain(top.iter())
-        .chain(right.iter())
-        .chain(bottom.iter())
-        .map(|f| **f)
-        .collect()
+fn lows(m: &Matrix<u8>) -> impl Iterator<Item = (usize, usize)> + '_ {
+    iproduct!(0..m.rows, 0..m.columns).filter(|&k| m.neighbours(k, false).all(|n| m[n] > m[k]))
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn neighbours() {
-        let test = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
-        // 1 2 3
-        // 4 5 6
-        // 7 8 9
-        assert_eq!(get_neighbours(&test, 0, 3), vec![2, 4]);
-        assert_eq!(get_neighbours(&test, 1, 3), vec![1, 3, 5]);
-        assert_eq!(get_neighbours(&test, 2, 3), vec![2, 6]);
-        assert_eq!(get_neighbours(&test, 3, 3), vec![1, 5, 7]);
-        assert_eq!(get_neighbours(&test, 4, 3), vec![4, 2, 6, 8]);
-        assert_eq!(get_neighbours(&test, 5, 3), vec![5, 3, 9]);
-        assert_eq!(get_neighbours(&test, 6, 3), vec![4, 8]);
-        assert_eq!(get_neighbours(&test, 7, 3), vec![7, 5, 9]);
-        assert_eq!(get_neighbours(&test, 8, 3), vec![8, 6]);
-    }
+fn part1(input: &str) -> u32 {
+    let m: Matrix<u8> = input.lines().map(|c| c.bytes()).collect();
+    lows(&m).map(|k| (m[k] - 48) as u32 + 1).sum()
+}
+
+fn part2(input: &str) -> usize {
+    let m = input.lines().map(|c| c.bytes()).collect();
+    lows(&m)
+        .map(|n| {
+            bfs_reach(n, |&n| {
+                m.neighbours(n, false)
+                    .filter(|&k| m[k] != 57 && m[k] > m[n])
+                    .collect_vec()
+            })
+            .count()
+        })
+        .sorted_unstable_by(|a, b| b.cmp(a))
+        .take(3)
+        .product()
 }
