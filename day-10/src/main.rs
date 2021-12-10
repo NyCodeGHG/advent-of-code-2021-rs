1d56b892bd1aeebf4fe3269e12d46f3658467d48
NyCode <me@nycode.dev>
2021-12-10 16:21:06 +0100

    Add solution for part 2 of day 10

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs;
 
@@ -15,9 +16,14 @@ macro_rules! collection {
 }
 
 fn main() {
+    let input = fs::read_to_string("inputs/day10.txt").expect("Unable to read input");
+    println!("{}", part_one(&input));
+    println!("{}", part_two(&input));
+}
+
+fn part_one(input: &str) -> u32 {
     let chars: HashMap<char, char> = collection! { ']' => '[', ')' => '(', '}' => '{', '>' => '<' };
-    let points: u32 = fs::read_to_string("inputs/day10.txt")
-        .expect("Unable to read input")
+    input
         .lines()
         .map(|line| {
             let mut chunks: Vec<char> = Vec::new();
@@ -45,6 +51,59 @@ fn main() {
             }
             0
         })
-        .sum();
-    println!("{}", points);
+        .sum()
+}
+
+fn part_two(input: &str) -> u64 {
+    let chars: HashMap<char, char> = collection! { '[' => ']', '(' => ')', '{' => '}', '<' => '>' };
+    let result = input
+        .lines()
+        .filter(|line| is_not_corrupted(line))
+        .map(|line| {
+            let mut chunks: Vec<char> = Vec::new();
+            for char in line.chars() {
+                if let Some(c) = chars.get(&char) {
+                    chunks.push(*c);
+                } else {
+                    chunks.pop();
+                }
+            }
+            chunks.reverse();
+            let result = chunks
+                .iter()
+                .map(|char| match *char {
+                    ')' => 1,
+                    ']' => 2,
+                    '}' => 3,
+                    '>' => 4,
+                    _ => 0,
+                })
+                .fold(0, |acc, value| acc * 5 + value);
+            result
+        })
+        .sorted()
+        .collect_vec();
+    *result.get(result.len() / 2).unwrap()
+}
+
+fn is_not_corrupted(line: &str) -> bool {
+    let chars: HashMap<char, char> = collection! { ']' => '[', ')' => '(', '}' => '{', '>' => '<' };
+    let mut chunks: Vec<char> = Vec::new();
+    for char in line.chars() {
+        let required = match chars.get(&char) {
+            Some(c) => c,
+            None => {
+                chunks.push(char);
+                continue;
+            }
+        };
+        let last = match chunks.pop() {
+            Some(c) => c,
+            None => panic!("No last value"),
+        };
+        if *required != last {
+            return false;
+        }
+    }
+    true
 }
